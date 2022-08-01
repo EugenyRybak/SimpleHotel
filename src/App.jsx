db692ebd58d2bb04169f76d9873012c04d09e3b9
John <nextadm@mail.ru>
2022-08-01 22:27:50 +0300

    beta 4.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { fetchHotels } from './store/reducers/hotels.reducer.js';
 import HotelPage from './components/hotelPage/HotelPage.jsx';
 import Login from './components/form/login/Login.jsx';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom'
 import { useEffect } from 'react';
 import './App.css';
@@ -9,6 +9,7 @@ import './App.css';
 function App() {
 
   const dispatch = useDispatch()
+  const test = useSelector((state) => state.user.isUser)
 
   useEffect(() => {
     dispatch(fetchHotels())
