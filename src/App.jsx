9384f11f161000e5550155175dd20461a00b2fd8
John <nextadm@mail.ru>
2022-08-01 22:10:28 +0300

    fixx

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,31 @@ import { fetchHotels } from './store/reducers/hotels.reducer.js';
 import HotelPage from './components/hotelPage/HotelPage.jsx';
 import Login from './components/form/login/Login.jsx';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
 import './App.css';
 
 function App() {
 
   const dispatch = useDispatch()
-  const test = useSelector((state) => state.user.isUser)
+  // const test = useSelector((state) => state.user.isUser)
+  // const navigate = useNavigate()
 
   useEffect(() => {
     dispatch(fetchHotels())
   }, [])
 
-  const redirect = () => {
-    if (window.location.pathname !== '/') window.location.replace('/')
-  }
+  // const redirect = () => {
+  //   if (window.location.pathname !== '/') window.location.replace('/')
+  //   navigate('/')
+  // }
 
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Login />} />
-        {test ? <Route path='/hotels' element={<HotelPage />} /> : <>{redirect()}</>}
+        {/* {test ? <Route path='/hotels' element={<HotelPage />} /> : <>{redirect()}</>} */}
+        <Route path='/hotels' element={<HotelPage />} /> 
       </Routes>
     </div>
   );
